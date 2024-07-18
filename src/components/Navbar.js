4518f776d18ba612eeca4ec63bdb365d0172ca6f
Aritra Kumar <121766655+aritrakumar@users.noreply.github.com>
2024-07-19 04:14:43 +0530

    Update github.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -97,7 +97,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/aritrakumar/Portfolio"
+                href="https://github.com/aritrakumar"
                 target="_blank"
                 className="fork-btn-inner"
               >
